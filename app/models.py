1a26eac9939353485645d4c26b8a52043e91a749
keshav <keshavsharma1196@gmail.com>
2023-08-04 13:23:03 +0530

    Restructrued the code into packages and modules

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    image_file = db.Column(db.String(30), nullable=False,
+                           default='default.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    date_posted = db.Column(db.DateTime(), nullable=False,
+                            default=datetime.utcnow())
+    content = db.Column(db.Text(), nullable=False)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
