2600dbaad65dfd0ce9a91f226292c9bfcb701bb2
keshav <keshavsharma1196@gmail.com>
2023-08-12 13:48:04 +0530

    moving creation of app into create_app func/to allow us to create diff instances of our application with diff configrations

--- app/__init__.py
+++ app/__init__.py
@@ -10,24 +10,45 @@ from app.config import Config
 
 load_dotenv()
 
-app = Flask(__name__)
+# app = Flask(__name__)
 # app.secret_key = os.environ.get('SECRET_KEY')
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 #     'SQLALCHEMY_DATABASE_URI')
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+# app.config.from_object(Config)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-mail = Mail(app)
+mail = Mail()
 
 # migrate=Migrate(app,db)
 
-from app.users.views import users
-from app.main.views import main
-from app.posts.views import posts
+# from app.users.views import users
+# from app.main.views import main
+# from app.posts.views import posts
 
-app.register_blueprint(users)
-app.register_blueprint(main)
-app.register_blueprint(posts)
+# app.register_blueprint(users)
+# app.register_blueprint(main)
+# app.register_blueprint(posts)
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    # migrate=Migrate(app,db)
+
+    from app.users.views import users
+    from app.main.views import main
+    from app.posts.views import posts
+
+    app.register_blueprint(users)
+    app.register_blueprint(main)
+    app.register_blueprint(posts)
+
+    return app
